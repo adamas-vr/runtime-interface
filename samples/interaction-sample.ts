21559b865027c3dccf4750e60067ea878c8af6f1
zekailin00 <zekailin00@gmail.com>
2025-07-29 21:02:20 -0700

    network interaction sample

--- samples/interaction-sample.ts
+++ samples/interaction-sample.ts
@@ -1,5 +1,6 @@
 import { EntityManager } from "@adamas/entity";
 import { GrabInteractableManager } from "@adamas/interaction/interaction";
+import { StateSync } from "@adamas/networking/state-sync";
 import {
 	MaterialManager,
 	ShaderProperties,
@@ -8,7 +9,7 @@ import {
 import { NewCubeMesh } from "@adamas/render/primitives";
 import { RenderableManager } from "@adamas/render/renderable";
 import { TransformManager } from "@adamas/render/transform";
-import { vec3, vec4 } from "gl-matrix";
+import { quat, vec3, vec4 } from "gl-matrix";
 
 export const createLocalGrabble = () => {
 	const entity = EntityManager.Create("network interactable");
@@ -73,4 +74,61 @@ export const createLocalGrabble = () => {
 			);
 		},
 	);
+};
+
+export const createNetworkGrabble = () => {
+	const entity = EntityManager.Create("network interactable");
+	TransformManager.SetLocalPosition(entity, vec3.fromValues(0, 3, 0));
+	const grabComp = GrabInteractableManager.Create(entity);
+	const renderComp = RenderableManager.Create(entity);
+	RenderableManager.SetMesh(entity, NewCubeMesh());
+	const material = MaterialManager.Create(ShaderType.URP_LIT);
+	RenderableManager.SetMaterial(entity, material);
+	MaterialManager.SetColor(
+		material,
+		ShaderProperties.BaseColor,
+		vec4.fromValues(0.2, 1.0, 0.2, 1.0),
+	);
+
+	const playerId = StateSync.GetPlayerId();
+	let networkProp = StateSync.CreateNetworkState(
+		"_internal::networkFollower",
+		{
+			grabbingClient: -1,
+			position: vec3.fromValues(0, 0, 0),
+			rotation: quat.identity(quat.fromValues(1, 0, 0, 0)),
+			time: "",
+		},
+		(prop, value) => {
+			if (networkProp.grabbingClient != playerId) {
+				console.log(`prop: ${prop}; value: ${value}`);
+				TransformManager.SetLocalPosition(entity, networkProp.position);
+				TransformManager.SetLocalRotation(entity, networkProp.rotation);
+			}
+		},
+	);
+
+	GrabInteractableManager.AddSelectEnteredCallback(
+		entity,
+		(interactable, interactor) => {
+			networkProp.grabbingClient = playerId;
+		},
+	);
+
+	GrabInteractableManager.AddSelectExitedCallback(
+		entity,
+		(interactable, interactor) => {
+			if (networkProp.grabbingClient == playerId) {
+				networkProp.grabbingClient = -1;
+			}
+		},
+	);
+
+	setInterval(() => {
+		if (networkProp.grabbingClient == playerId) {
+			networkProp.position = TransformManager.GetLocalPosition(entity);
+			networkProp.rotation = TransformManager.GetLocalRotation(entity);
+			networkProp.time = new Date().toISOString();
+		}
+	}, 200);
 };
