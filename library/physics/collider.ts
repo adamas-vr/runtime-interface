1b863c673cb479dd4db23221db0f58d9eaad84cb
zekailin00 <zekailin00@gmail.com>
2025-08-01 14:54:37 -0700

    collider shape API

--- library/physics/collider.ts
+++ library/physics/collider.ts
@@ -1,5 +1,6 @@
 import { Entity } from "@adamas/entity";
 import { RpcClient } from "@adamas/rpc";
+import { vec3 } from "gl-matrix";
 
 export type ColliderHandle = number;
 
@@ -58,6 +59,142 @@ export class ColliderManager {
 				entityHandle,
 			}),
 		);
+	}
+
+	static GetBoxColliderCenter(colliderHandle: ColliderHandle): vec3 {
+		const center = JSON.parse(
+			RpcClient.Call("ColliderAPI_GetBoxColliderCenter", {
+				colliderHandle,
+			}) as string,
+		) as number[];
+
+		return vec3.fromValues(center[0], center[1], center[2]);
+	}
+
+	static SetBoxColliderCenter(
+		colliderHandle: ColliderHandle,
+		center: vec3,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetBoxColliderCenter", {
+			colliderHandle,
+			v0: center[0],
+			v1: center[1],
+			v2: center[2],
+		});
+	}
+
+	static GetBoxColliderSize(colliderHandle: ColliderHandle): vec3 {
+		const center = JSON.parse(
+			RpcClient.Call("ColliderAPI_GetBoxColliderSize", {
+				colliderHandle,
+			}) as string,
+		) as number[];
+
+		return vec3.fromValues(center[0], center[1], center[2]);
+	}
+
+	static SetBoxColliderSize(
+		colliderHandle: ColliderHandle,
+		size: vec3,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetBoxColliderSize", {
+			colliderHandle,
+			v0: size[0],
+			v1: size[1],
+			v2: size[2],
+		});
+	}
+
+	static GetSphereColliderCenter(colliderHandle: ColliderHandle): vec3 {
+		const center = JSON.parse(
+			RpcClient.Call("ColliderAPI_GetSphereColliderCenter", {
+				colliderHandle,
+			}) as string,
+		) as number[];
+
+		return vec3.fromValues(center[0], center[1], center[2]);
+	}
+
+	static SetSphereColliderCenter(
+		colliderHandle: ColliderHandle,
+		center: vec3,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetSphereColliderCenter", {
+			colliderHandle,
+			v0: center[0],
+			v1: center[1],
+			v2: center[2],
+		});
+	}
+
+	static GetSphereColliderRadius(colliderHandle: ColliderHandle): number {
+		return RpcClient.Call("ColliderAPI_GetSphereColliderRadius", {
+			colliderHandle,
+		}) as number;
+	}
+
+	static SetSphereColliderRadius(
+		colliderHandle: ColliderHandle,
+		radius: number,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetSphereColliderRadius", {
+			colliderHandle,
+			radius,
+		});
+	}
+
+	static GetCapsuleColliderCenter(colliderHandle: ColliderHandle): vec3 {
+		const center = JSON.parse(
+			RpcClient.Call("ColliderAPI_GetCapsuleColliderCenter", {
+				colliderHandle,
+			}) as string,
+		) as number[];
+
+		return vec3.fromValues(center[0], center[1], center[2]);
+	}
+
+	static SetCapsuleColliderCenter(
+		colliderHandle: ColliderHandle,
+		center: vec3,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetCapsuleColliderCenter", {
+			colliderHandle,
+			v0: center[0],
+			v1: center[1],
+			v2: center[2],
+		});
+	}
+
+	static GetCapsuleColliderRadius(colliderHandle: ColliderHandle): number {
+		return RpcClient.Call("ColliderAPI_GetCapsuleColliderRadius", {
+			colliderHandle,
+		}) as number;
+	}
+
+	static SetCapsuleColliderRadius(
+		colliderHandle: ColliderHandle,
+		radius: number,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetCapsuleColliderRadius", {
+			colliderHandle,
+			radius,
+		});
+	}
+
+	static GetCapsuleColliderHeight(colliderHandle: ColliderHandle): number {
+		return RpcClient.Call("ColliderAPI_GetCapsuleColliderHeight", {
+			colliderHandle,
+		}) as number;
+	}
+
+	static SetCapsuleColliderHeight(
+		colliderHandle: ColliderHandle,
+		height: number,
+	): boolean {
+		return RpcClient.Call("ColliderAPI_SetCapsuleColliderHeight", {
+			colliderHandle,
+			height,
+		});
 	}
 
 	static GetIsTrigger(entityHandle: Entity): Boolean {
