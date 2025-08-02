32f124aabd51f29e8d4491b1908f1d4e6f1d184f
zekailin00 <zekailin00@gmail.com>
2025-08-02 01:32:43 -0700

    samples

--- index.ts
+++ index.ts
@@ -1,22 +1,35 @@
-import { EntityManager } from "@adamas/entity";
+import { Entity } from "@adamas/entity";
 import { GrabInteractableManager } from "@adamas/interaction/interaction";
-import { StateSync } from "@adamas/networking/state-sync";
-import { CameraManager } from "@adamas/render/camera";
-import {
-	MaterialManager,
-	ShaderProperties,
-	ShaderType,
-} from "@adamas/render/material";
-import { NewCubeMesh, NewQuadMesh } from "@adamas/render/primitives";
-import { RenderableManager } from "@adamas/render/renderable";
-import { TextureFormat, TextureManager } from "@adamas/render/texture";
+import { ColliderManager, ColliderHandle } from "@adamas/physics/collider";
+import { RigidbodyManager } from "@adamas/physics/rigidbody";
 import { TransformManager } from "@adamas/render/transform";
-import { vec3, vec4 } from "gl-matrix";
-import { createLocalGrabble } from "samples/interaction-sample";
+import { vec3 } from "gl-matrix";
+import { cameraRenderTexture, renderGltf } from "samples/rendering-sample";
+
+const heartModel = async () => {
+	const modelEntity: Entity = await renderGltf("realistic_human_heart.glb");
+	TransformManager.SetLocalPosition(modelEntity, vec3.fromValues(1, 1, 0));
+	TransformManager.SetLocalScale(modelEntity, vec3.fromValues(0.2, 0.2, 0.2));
+	RigidbodyManager.Create(modelEntity);
+	RigidbodyManager.SetIsKinematic(modelEntity, true);
+	RigidbodyManager.SetUseGravity(modelEntity, false);
+	const collider = ColliderManager.CreateCapsule(modelEntity);
+	ColliderManager.SetCapsuleColliderRadius(collider, 1.0);
+	ColliderManager.SetCapsuleColliderHeight(collider, 3.0);
+	GrabInteractableManager.Create(modelEntity);
+};
+
+const surgerySim = async () => {
+	const modelEntity: Entity = await renderGltf("hospital-bed.glb");
+	TransformManager.SetLocalPosition(modelEntity, vec3.fromValues(0, 0.5, 0));
+};
+
+surgerySim();
+
+// cameraRenderTexture();
+// heartModel();
 
 console.log(process.cwd());
 setInterval(() => {
 	console.log("Ticked!");
 }, 30000);
-
-createLocalGrabble();
