496be8fca6c3d387fa9625214abdd115bfe64531
zekailin00 <zekailin00@gmail.com>
2025-09-12 13:30:25 -0700

    vscode intellisense support

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { Entity } from "@adamas/entity";
+
 import { GrabInteractableManager } from "@adamas/interaction/interaction";
 import { ColliderManager, ColliderHandle } from "@adamas/physics/collider";
 import { RigidbodyManager } from "@adamas/physics/rigidbody";
 import { TransformManager } from "@adamas/render/transform";
-import { vec3 } from "gl-matrix";
+import { vec3 } from "@adamas/gl-matrix";
 import { cameraRenderTexture, renderGltf } from "samples/rendering-sample";
 
 const heartModel = async () => {
