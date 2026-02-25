388524e293a878d71c4465ddb64f8b4139495868
zekailin00 <zekailin00@gmail.com>
2026-02-24 21:06:39 -0800

    finalize transform

--- library/render/transform.ts
+++ library/render/transform.ts
@@ -1,81 +1,30 @@
 import { RpcClient } from "../rpc";
 import { Entity } from "../entity";
-import { mat4, quat, vec3 } from "gl-matrix";
+import { quat, vec3 } from "gl-matrix";
 
-/**
- * Wrapper for Unity Transform RPCs.
- * Provides strongly-typed parameters and returns.
- */
 export class TransformManager {
 	/**
-	 * Checks if the entity has a Transform component.
-	 * @param entity The entity to check
-	 * @returns boolean indicating if transform component exists
-	 */
-	static HasComponent(entity: Entity): boolean {
-		return Boolean(
-			RpcClient.Call("Transform_HasComponent", { entityHandle: entity }),
-		);
-	}
-
-	/**
-	 * Sets the world transform given a 16-element row-major matrix.
-	 * Transposes to column-major and flips Z-axis for Unity.
-	 * TODO: double check if this is correct, what are we using internally?
-	 * Use the coords provided by gl matrix or unity.
+	 * Rotates the transform so the entity looks at the world position.
 	 * @param entity The entity with the transform component
-	 * @param srcMatrix The source transformation matrix
+	 * @param worldPosition The world position to look at.
+	 * @param worldUp The world upward direction.
 	 * @returns boolean indicating success
 	 */
-	static SetTransform(entity: Entity, srcMatrix: mat4): boolean {
-		const colMaj: number[] = new Array(16);
-		for (let row = 0; row < 4; row++) {
-			for (let col = 0; col < 4; col++) {
-				let val = srcMatrix[row * 4 + col];
-				if (col === 2) val = -val; // flip Z-axis
-				colMaj[col * 4 + row] = val;
-			}
-		}
-		const matrixJson = colMaj.join(",");
+	static LookAt(
+		entity: Entity,
+		worldPosition: vec3,
+		worldUp: vec3 = vec3.fromValues(0, 1, 0),
+	): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetTransform", {
+			RpcClient.Call("Transform::LookAt", {
 				entityHandle: entity,
-				matrixJson,
+				posX: worldPosition[0],
+				posY: worldPosition[1],
+				posZ: worldPosition[2],
+				upX: worldUp[0],
+				upY: worldUp[1],
+				upZ: worldUp[2],
 			}),
-		);
-	}
-
-	/**
-	 * Retrieves the world transform matrix (column-major).
-	 * @param entity The entity with the transform component
-	 * @returns The transformation matrix
-	 */
-	static GetTransform(entity: Entity): mat4 {
-		const result = RpcClient.Call("Transform_GetTransform", {
-			entityHandle: entity,
-		}) as string;
-		const nums = result.split(",").map((j) => parseFloat(j));
-
-		if (nums.length !== 16) {
-			throw new Error(`Invalid transform data (${nums.length} elements)`);
-		}
-		return mat4.fromValues(
-			nums[0],
-			nums[1],
-			nums[2],
-			nums[3],
-			nums[4],
-			nums[5],
-			nums[6],
-			nums[7],
-			nums[8],
-			nums[9],
-			nums[10],
-			nums[11],
-			nums[12],
-			nums[13],
-			nums[14],
-			nums[15],
 		);
 	}
 
@@ -85,9 +34,9 @@ export class TransformManager {
 	 * @param parent The parent entity
 	 * @returns boolean indicating success
 	 */
-	static SetParent(entity: Entity, parent: Entity): boolean {
+	static SetParent(entity: Entity, parent: Entity = -1): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetParent", {
+			RpcClient.Call("Transform::SetParent", {
 				processId: process.pid,
 				entityHandle: entity,
 				parentHandle: parent,
@@ -96,14 +45,20 @@ export class TransformManager {
 	}
 
 	/**
-	 * Gets the parent entity handle, or -1 if none.
+	 * Gets the parent entity handle, or undefined if none.
 	 * @param entity The entity with the transform component
-	 * @returns The parent entity handle
+	 * @returns The parent entity handle or undefined
 	 */
-	static GetParent(entity: Entity): Entity {
-		return Number(
-			RpcClient.Call("Transform_GetParent", { entityHandle: entity }),
+	static GetParent(entity: Entity): Entity | undefined {
+		const parent = Number(
+			RpcClient.Call("Transform::GetParent", {
+				processId: process.pid,
+				entityHandle: entity,
+			}),
 		);
+
+		if (parent == -1) return undefined;
+		return parent;
 	}
 
 	/**
@@ -114,7 +69,7 @@ export class TransformManager {
 	 */
 	static SetWorldPosition(entity: Entity, pos: vec3): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetWorldPosition", {
+			RpcClient.Call("Transform::SetWorldPosition", {
 				entityHandle: entity,
 				x: pos[0],
 				y: pos[1],
@@ -131,7 +86,7 @@ export class TransformManager {
 	 */
 	static SetLocalPosition(entity: Entity, pos: vec3): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetLocalPosition", {
+			RpcClient.Call("Transform::SetLocalPosition", {
 				entityHandle: entity,
 				x: pos[0],
 				y: pos[1],
@@ -146,7 +101,7 @@ export class TransformManager {
 	 * @returns The position vector
 	 */
 	static GetWorldPosition(entity: Entity): vec3 {
-		const data = RpcClient.Call("Transform_GetWorldPosition", {
+		const data = RpcClient.Call("Transform::GetWorldPosition", {
 			entityHandle: entity,
 		}) as string;
 		const [x, y, z] = data.split(",").map((j) => parseFloat(j));
@@ -159,7 +114,7 @@ export class TransformManager {
 	 * @returns The position vector
 	 */
 	static GetLocalPosition(entity: Entity): vec3 {
-		const data = RpcClient.Call("Transform_GetLocalPosition", {
+		const data = RpcClient.Call("Transform::GetLocalPosition", {
 			entityHandle: entity,
 		}) as string;
 		const [x, y, z] = data.split(",").map((j) => parseFloat(j));
@@ -174,7 +129,7 @@ export class TransformManager {
 	 */
 	static SetWorldRotation(entity: Entity, quat: quat): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetWorldRotation", {
+			RpcClient.Call("Transform::SetWorldRotation", {
 				entityHandle: entity,
 				x: quat[0],
 				y: quat[1],
@@ -192,7 +147,7 @@ export class TransformManager {
 	 */
 	static SetLocalRotation(entity: Entity, quat: quat): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetLocalRotation", {
+			RpcClient.Call("Transform::SetLocalRotation", {
 				entityHandle: entity,
 				x: quat[0],
 				y: quat[1],
@@ -208,7 +163,7 @@ export class TransformManager {
 	 * @returns The rotation quaternion
 	 */
 	static GetWorldRotation(entity: Entity): quat {
-		const data = RpcClient.Call("Transform_GetWorldRotation", {
+		const data = RpcClient.Call("Transform::GetWorldRotation", {
 			entityHandle: entity,
 		}) as string;
 		const [x, y, z, w] = data.split(",").map((j) => parseFloat(j));
@@ -221,7 +176,7 @@ export class TransformManager {
 	 * @returns The rotation quaternion
 	 */
 	static GetLocalRotation(entity: Entity): quat {
-		const data = RpcClient.Call("Transform_GetLocalRotation", {
+		const data = RpcClient.Call("Transform::GetLocalRotation", {
 			entityHandle: entity,
 		}) as string;
 		const [x, y, z, w] = data.split(",").map((j) => parseFloat(j));
@@ -236,7 +191,7 @@ export class TransformManager {
 	 */
 	static SetLocalScale(entity: Entity, scale: vec3): boolean {
 		return Boolean(
-			RpcClient.Call("Transform_SetLocalScale", {
+			RpcClient.Call("Transform::SetLocalScale", {
 				entityHandle: entity,
 				x: scale[0],
 				y: scale[1],
@@ -251,7 +206,7 @@ export class TransformManager {
 	 * @returns The position vector
 	 */
 	static GetLocalScale(entity: Entity): vec3 {
-		const data = RpcClient.Call("Transform_GetLocalScale", {
+		const data = RpcClient.Call("Transform::GetLocalScale", {
 			entityHandle: entity,
 		}) as string;
 		const [x, y, z] = data.split(",").map((j) => parseFloat(j));
