3433ce2966acb71de41401da9a3398f2db89024a
zekailin00 <zekailin00@gmail.com>
2026-03-03 16:11:15 -0800

    user api in progress

--- library/player.ts
+++ library/player.ts
@@ -1,14 +1,29 @@
 import { quat, vec3 } from "gl-matrix";
 import { Entity } from "./entity";
+import { RpcClient } from "./rpc";
 
 /**
- * TODO: need to return player?
+ * TODO: need to return user?
  * grabble on select
  * trigger: on collider
  */
 
-export class Player {
-	#playerId: number = -1;
+export class User {
+	constructor(private userId: string) {}
+
+	static GetUserById(userId: string): User | undefined {
+		const user = new User(userId);
+
+		if (user.IsValid()) return user;
+		return undefined;
+	}
+	static GetUsers(): User[] {
+		const userIds = RpcClient.Call("UserAPI::GetUsers", {}) as string[];
+		return userIds.map((userId) => new User(userId));
+	}
+	GetUserId(): string {
+		return this.userId;
+	}
 
 	GetHeadEntity(): Entity {
 		return 0;
@@ -19,75 +34,107 @@ export class Player {
 	GetRightHandEntity(): Entity {
 		return 0;
 	}
-	GetPlayerRootEntity(): Entity {
+	GetUserRootEntity(): Entity {
 		return 0;
 	}
 
-	GetPlayerWorldPosition(): vec3 {
-		return vec3.fromValues(0, 0, 0);
+	SetEnableLocomotion(value: boolean): void {
+		RpcClient.Call("UserAPI::SetEnableLocomotion", {
+			userId: this.userId,
+			value,
+		});
 	}
-	GetPlayerWorldRotation(): quat {
-		return quat.fromValues(0, 0, 0, 0);
+	GetEnableLocomotion(): boolean {
+		return RpcClient.Call("UserAPI::GetEnableLocomotion", {
+			userId: this.userId,
+		}) as boolean;
+	}
+	TeleportTo(worldPosition: vec3, worldRotation: quat): void {
+		RpcClient.Call("UserAPI::TeleportTo", {
+			userId: this.userId,
+			worldPosition: JSON.stringify(worldPosition),
+			worldRotation: JSON.stringify(worldRotation),
+		});
+	}
+	GetUserWorldPosition(): vec3 {
+		const pos = RpcClient.Call("UserAPI::GetUserWorldPosition", {
+			userId: this.userId,
+		});
+		return pos.fromValues(pos[0], pos[1], pos[2]);
+	}
+	GetUserWorldRotation(): quat {
+		const rot = RpcClient.Call("UserAPI::GetUserWorldRotation", {
+			userId: this.userId,
+		});
+		return quat.fromValues(rot[0], rot[1], rot[2], rot[3]);
 	}
 
 	IsValid(): boolean {
-		return false;
+		return RpcClient.Call("UserAPI::IsValid", {
+			userId: this.userId,
+		}) as boolean;
 	}
 	IsLocal(): boolean {
-		return true;
+		return RpcClient.Call("UserAPI::IsLocal", {
+			userId: this.userId,
+		}) as boolean;
 	}
-	IsUserInVR(): boolean {
-		return false;
+	IsVRMode(): boolean {
+		return RpcClient.Call("UserAPI::IsVRMode", {
+			userId: this.userId,
+		}) as boolean;
 	}
 	IsMenuOpen(): boolean {
-		return false;
+		return RpcClient.Call("UserAPI::IsMenuOpen", {
+			userId: this.userId,
+		}) as boolean;
 	}
 	IsGrounded(): boolean {
-		return false;
+		return RpcClient.Call("UserAPI::IsGrounded", {
+			userId: this.userId,
+		}) as boolean;
 	}
 	IsFlying(): boolean {
-		return false;
+		return RpcClient.Call("UserAPI::IsFlying", {
+			userId: this.userId,
+		}) as boolean;
 	}
-
-	SetEnableFly(value: boolean) {}
-	GetEnableFly(): boolean {
-		return false;
-	}
-
-	SetEnableLocomotion(value: boolean) {}
-	GetEnableLocomotion(): boolean {
-		return false;
-	}
-	TeleportTo(worldPosition: vec3, worldRotation: quat): void {}
 
 	GetMoveSpeed(): number {
-		return 0;
+		return RpcClient.Call("UserAPI::GetMoveSpeed", {
+			userId: this.userId,
+		}) as number;
 	}
-	SetMoveSpeed(speed: number): void {}
-
-	GetJumpImpulse(): number {
-		return 0;
+	SetMoveSpeed(speed: number): void {
+		RpcClient.Call("UserAPI::SetMoveSpeed", {
+			userId: this.userId,
+			speed,
+		});
 	}
-	SetJumpImpulse(impulse: number) {}
-
+	GetJumpVelocity(): number {
+		return RpcClient.Call("UserAPI::GetJumpVelocity", {
+			userId: this.userId,
+		}) as number;
+	}
+	SetJumpVelocity(velocity: number) {
+		RpcClient.Call("UserAPI::GetJumpVelocity", {
+			userId: this.userId,
+			velocity,
+		});
+	}
 	GetGravityStrength(): number {
-		return 0;
+		return RpcClient.Call("UserAPI::GetGravityStrength", {
+			userId: this.userId,
+		}) as number;
 	}
-	SetGravityStrength(strength: number) {}
+	SetGravityStrength(strength: number) {
+		RpcClient.Call("UserAPI::SetGravityStrength", {
+			userId: this.userId,
+			strength,
+		});
+	}
 
-	GetPlayerInfo() {}
-	GetPlayerId(): number {
-		return this.#playerId;
-	}
-
-	OnPlayerTriggerEnter() {}
-	OnPlayerTriggerStay() {}
-	OnPlayerTriggerExit() {}
-
-	static GetPlayers(): Player[] {
-		return [];
-	}
-	static GetPlayerById(playerId: number): Player | undefined {
-		return undefined;
-	}
+	OnUserTriggerEnter() {}
+	OnUserTriggerStay() {}
+	OnUserTriggerExit() {}
 }
